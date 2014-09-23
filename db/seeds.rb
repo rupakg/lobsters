76d84f7443a890c78951aeac3eff45222eb01ef0
Rupak Ganguly <rupakg@gmail.com>
2014-09-23 18:58:37 -0400

    Add some seed data, and update config.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,16 @@
-# Create a default user
-u = User.new(:username => 'test', :email => 'test@example.com', :password => 'test', :password_confirmation => 'test')
+# Create a default admin user
+u = User.new(:username => 'superadmin', :email => 'admin@example.com', :password => 'test', :password_confirmation => 'test')
 u.is_admin = true
 u.is_moderator = true
 u.save
 
-# Create a default tag
-t = Tag.new
-t.tag = 'test'
-t.save
+# create a non-admin user
+u = User.create(:username => 'dev', :email => 'dev@example.com', :password => 'test', :password_confirmation => 'test')
+
+# Create some default tags
+Tag.create(tag: 'docker')
+Tag.create(tag: 'paas')
+Tag.create(tag: 'panamax')
+Tag.create(tag: 'centurylink')
+Tag.create(tag: 'kubernetes')
+Tag.create(tag: 'mesos')
